faf7cc692675836fd21028f962aeb79304c00ec1
Matěj <mckillem@tuta.io>
2023-11-06 08:58:02 +0100

    stylování

--- client/src/pages/Header.tsx
+++ client/src/pages/Header.tsx
@@ -2,13 +2,15 @@ import "./Header.scss";
 export const Header = () => {
 	return (
 		<div id="header">
-			<img src="" alt=""/>
-			csfd
-			<form>
-				<input type="text" placeholder="Vyhledávání"/>
-				<button>Vyhledej</button>
-			</form>
-			<a href="/">Podrobné vyhledávání</a>
+			<img src="../logo/logos-png-8.png" alt="logo"/>
+			{/*<img src="https://img.csfd.cz/documents/marketing/logos/logo-white-red/logo-white-red.svg" alt=""/>*/}
+			<div className="search">
+				<form>
+					<input type="text" placeholder="Vyhledávání"/>
+					<button><img src="../logo/lupa.png" alt="lupa"/></button>
+				</form>
+				{/*<a href="/">Podrobné vyhledávání</a>*/}
+			</div>
 			<div>Můj účet</div>
 		</div>
 	);
